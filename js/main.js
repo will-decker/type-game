e9887f7ae8c673eb945092b4c4e3bb6cfca68b1e
William Decker <willdecker@gmail.com>
2018-08-08 19:24:07 -0400

    completed all basic game functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 window.addEventListener('load', init);
 
-// Globals
-let time = 5;
+// Levels
+const levels = {
+  easy: 5,
+  medium: 3,
+  hard: 2
+}
+
+const currentLevel = levels.easy;
+
+let time = currentLevel;
 let score = 0;
 let isPlaying;
 
 // DOM Elements
-
 const wordInput = document.getElementById('word-input');
 const currentWord = document.getElementById('current-word');
-const scoreDisplay = document.getElementById('#score');
+const scoreDisplay = document.getElementById('score');
 const timeDisplay = document.getElementById('time');
 const message = document.getElementById('message');
 const seconds = document.getElementById('seconds');
@@ -44,11 +51,43 @@ const words = [
 
 // Initialize Game
 function init() {
+  //  SHow number of seconds in UI
+  seconds.innerHTML = currentLevel;
   // Load word from array
   showWord(words);
+  // Sart matching on word input
+  wordInput.addEventListener('input', startMatch);
   //  Call countdown every second
   setInterval(countdown, 1000);
   setInterval(checkStatus, 50);
+}
+
+// Start match
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord(words);
+    wordInput.value = '';
+    score++;
+  }
+
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+
+// Match currentWord to wordInput
+function matchWords() {
+  if (wordInput.value === currentWord.innerHTML) {
+    message.innerHTML = 'Correct!';
+    return true;
+  } else {
+    message.innerHTML = '';
+    return false;
+  }
 }
 
 // Pick and show random word
@@ -76,5 +115,6 @@ function countdown() {
 function checkStatus() {
   if (!isPlaying && time === 0) {
     message.innerHTML = 'Game Over!';
+    score = -1;
   }
 }
